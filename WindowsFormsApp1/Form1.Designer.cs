06f40bdf740f8069f2b3e4e0031b754d57bad584
Sari <divinemidnight@gmail.com>
2017-04-27 17:29:37 +0300

    Project initial upload

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.create_button = new System.Windows.Forms.Button();
+            this.textBoxStiches = new System.Windows.Forms.TextBox();
+            this.userInputForStiches = new System.Windows.Forms.TextBox();
+            this.textBoxRows = new System.Windows.Forms.TextBox();
+            this.userInputForRows = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.clear_button = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // create_button
+            // 
+            this.create_button.Font = new System.Drawing.Font("Arial Rounded MT Bold", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.create_button.Location = new System.Drawing.Point(22, 471);
+            this.create_button.Name = "create_button";
+            this.create_button.Size = new System.Drawing.Size(229, 63);
+            this.create_button.TabIndex = 3;
+            this.create_button.Text = "C R E A T E";
+            this.create_button.UseVisualStyleBackColor = true;
+            this.create_button.Click += new System.EventHandler(this.create_button_Click);
+            // 
+            // textBoxStiches
+            // 
+            this.textBoxStiches.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxStiches.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxStiches.Location = new System.Drawing.Point(12, 280);
+            this.textBoxStiches.Name = "textBoxStiches";
+            this.textBoxStiches.Size = new System.Drawing.Size(102, 25);
+            this.textBoxStiches.TabIndex = 4;
+            this.textBoxStiches.Text = "STICHES";
+            this.textBoxStiches.MouseHover += new System.EventHandler(this.textBoxStiches_MouseHover);
+            // 
+            // userInputForStiches
+            // 
+            this.userInputForStiches.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userInputForStiches.Location = new System.Drawing.Point(120, 277);
+            this.userInputForStiches.Name = "userInputForStiches";
+            this.userInputForStiches.Size = new System.Drawing.Size(70, 32);
+            this.userInputForStiches.TabIndex = 5;
+            // 
+            // textBoxRows
+            // 
+            this.textBoxRows.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxRows.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxRows.Location = new System.Drawing.Point(12, 370);
+            this.textBoxRows.Multiline = true;
+            this.textBoxRows.Name = "textBoxRows";
+            this.textBoxRows.Size = new System.Drawing.Size(76, 31);
+            this.textBoxRows.TabIndex = 6;
+            this.textBoxRows.Text = "ROWS";
+            this.textBoxRows.MouseHover += new System.EventHandler(this.textBoxRows_MouseHover);
+            // 
+            // userInputForRows
+            // 
+            this.userInputForRows.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userInputForRows.Location = new System.Drawing.Point(22, 407);
+            this.userInputForRows.Name = "userInputForRows";
+            this.userInputForRows.Size = new System.Drawing.Size(56, 32);
+            this.userInputForRows.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(264, 55);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Hello user!";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle4.Padding = new System.Windows.Forms.Padding(1);
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.ControlDark;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle5.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle5.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.ControlDark;
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle5;
+            this.dataGridView1.GridColor = System.Drawing.Color.DarkGray;
+            this.dataGridView1.Location = new System.Drawing.Point(336, 12);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle6.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle6.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle6.Padding = new System.Windows.Forms.Padding(1);
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.SystemColors.ControlDark;
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle6;
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dataGridView1.RowTemplate.Height = 4;
+            this.dataGridView1.RowTemplate.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.Size = new System.Drawing.Size(836, 600);
+            this.dataGridView1.TabIndex = 9;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 145);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(264, 116);
+            this.textBox1.TabIndex = 11;
+            this.textBox1.Text = "Please enter the size of the pattern you want to create";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(111, 315);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(161, 132);
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // clear_button
+            // 
+            this.clear_button.Font = new System.Drawing.Font("Arial Rounded MT Bold", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clear_button.Location = new System.Drawing.Point(22, 549);
+            this.clear_button.Name = "clear_button";
+            this.clear_button.Size = new System.Drawing.Size(229, 63);
+            this.clear_button.TabIndex = 13;
+            this.clear_button.Text = "C L E A R";
+            this.clear_button.UseVisualStyleBackColor = true;
+            this.clear_button.Click += new System.EventHandler(this.clear_button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1184, 661);
+            this.Controls.Add(this.clear_button);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.userInputForRows);
+            this.Controls.Add(this.textBoxRows);
+            this.Controls.Add(this.userInputForStiches);
+            this.Controls.Add(this.textBoxStiches);
+            this.Controls.Add(this.create_button);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Create a Pattern";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Leave += new System.EventHandler(this.Form1_Leave);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button create_button;
+        private System.Windows.Forms.TextBox textBoxStiches;
+        private System.Windows.Forms.TextBox userInputForStiches;
+        private System.Windows.Forms.TextBox textBoxRows;
+        private System.Windows.Forms.TextBox userInputForRows;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button clear_button;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,186 @@
+﻿/*******************************************************************
+ * Coding sample                                                   *
+ * Created for Laavu Solutions                                     *
+ * Coded by Sari Alila                                             *
+ * Date: 27.4.2017                                                 *
+ *******************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        //Limiting the amount of stiches and row to keep the program small
+        //Can be easily changed here for the entire program
+        int maxStiches = 30;
+        int maxRows = 25;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void create_button_Click(object sender, EventArgs e)
+        {
+            //Test to see if reading user input works
+            //MessageBox.Show(this.userInputForStiches.Text + " and " + this.userInputForRows.Text);
+
+            //int variables for the stiches and rows, which are acquired from the user
+            //set to zero
+            int userDefinedStiches = 0;
+            int userDefinedRows = 0;
+
+            //Try parsing the text from the two TextBoxes to int
+            //If the input is not a number, show message prompting to enter a number (int)
+
+            /************* FOR TESTING*********************
+            String silmukat = userInputForStiches.Text;
+            MessageBox.Show("silmukat:" + silmukat);
+            bool IsStichesANumber = Int32.TryParse(silmukat, out userDefinedStiches);
+            MessageBox.Show("bool:" + IsStichesANumber);
+            ************ FOR TESTING**********************/
+
+
+            //Checking if the given number is valid.
+            //The number cannot be negative, nor zero. The number cannot be too large either
+            //Show message prompting to check the number with some instructions
+            if (!(Int32.TryParse(userInputForStiches.Text, out userDefinedStiches)) || userDefinedStiches <= 0 || userDefinedStiches > maxStiches)
+            {
+                MessageBox.Show("Please, check the STICH count. It needs to be a number below " + maxStiches + ", not negative, nor zero.");
+            }
+
+            else if (!(Int32.TryParse(userInputForRows.Text, out userDefinedRows)) || userDefinedRows <= 0 || userDefinedRows > maxRows)
+            {
+                MessageBox.Show("Please, check the ROW count. It needs to be a number below " + maxRows + ", not negative, nor zero.");
+            }
+
+            //If everything is OK with the user input (numbers), continue program
+            else
+            {
+                //User's stitches = colums
+                //and user's rows = rows
+                dataGridView1.ColumnCount = userDefinedStiches;
+                dataGridView1.RowCount = userDefinedRows;
+
+                //Set the column header names (in this case numbers).
+                for (int y = 0; y < dataGridView1.ColumnCount; ++y)
+                {
+                    dataGridView1.Columns[y].Name = (y + 1).ToString();
+                }
+
+
+                // Set the row header width.
+                //Header width can be larger than normal cell's width without distrupting knitting grid ratio [4:5]
+                dataGridView1.RowHeadersWidth = 70;
+
+                // Set the row header names (in this case numbers).
+                for (int y = 0; y < dataGridView1.RowCount; ++y)
+                {
+                    dataGridView1.Rows[y].HeaderCell.Value = (y + 1).ToString();
+
+                }
+
+                // Set the column width.
+                for (int y = 0; y < dataGridView1.ColumnCount; ++y)
+                {
+                    dataGridView1.Columns[y].Width = 40;
+                }
+
+                // Set the row height.
+                for (int y = 0; y < dataGridView1.RowCount; ++y)
+                {
+                    dataGridView1.Rows[y].Height = 32;
+                }
+
+                //Set the background colour of the cells white at first
+                foreach (DataGridViewRow row in dataGridView1.Rows)
+                {
+                    foreach (DataGridViewColumn col in dataGridView1.Columns)
+                    {
+                        dataGridView1.Rows[row.Index].Cells[col.Index].Style.BackColor = Color.White;
+                    }
+                }
+            }
+        }
+
+        //When a cell is clicked
+        private void DataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //Checking if the clicked cell is row or column header
+            if (e.ColumnIndex < 0 || e.RowIndex < 0)
+            { }
+
+            //continue checking the colour
+            else
+            {
+                //Checking if the clicked cell is black or white.
+                //When it's black, reset it to white and vice versa
+                if (dataGridView1[e.ColumnIndex, e.RowIndex].Style.BackColor == Color.Black)
+                {
+                    //For testing to see if the colour changes work
+                    //MessageBox.Show("on musta, maalataan valkoiseksi");
+
+                    dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Style.BackColor = Color.White;
+                }
+
+                else
+                {
+                    //For testing to see if the colour changes work
+                    //MessageBox.Show("On valkoinen, maalataan mustaksi");
+                    dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Style.BackColor = Color.Black;
+                }
+            }
+
+        }
+
+        private void clear_button_Click(object sender, EventArgs e)
+        {
+            //Clear button which empties the grid
+            dataGridView1.ColumnCount = 0;
+            dataGridView1.RowCount = 0;
+            dataGridView1.DefaultCellStyle.BackColor = Color.White;
+        }
+
+        private void Form1_Leave(object sender, EventArgs e)
+        {
+            dataGridView1.Dispose();
+            
+        }
+        //Show info about the max value when the mouse is hovering over the textbox (for stiches)
+        private void textBoxStiches_MouseHover(object sender, EventArgs e)
+        {
+
+            System.Windows.Forms.ToolTip myToolTip = new System.Windows.Forms.ToolTip();
+            myToolTip.ToolTipIcon = ToolTipIcon.Info;
+            myToolTip.ShowAlways = true;
+            myToolTip.SetToolTip(textBoxStiches, "The maximum is " + maxStiches);
+            
+        }
+
+        //Show info about the max value when the mouse is hovering over the textbox (for rows)
+        private void textBoxRows_MouseHover(object sender, EventArgs e)
+        {
+            System.Windows.Forms.ToolTip myToolTip = new System.Windows.Forms.ToolTip();
+            myToolTip.ToolTipIcon = ToolTipIcon.Info;
+            myToolTip.ShowAlways = true;
+            myToolTip.SetToolTip(textBoxRows, "The maximum is " + maxRows);
+        }
+    }
+}
